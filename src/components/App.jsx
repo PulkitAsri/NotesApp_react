cb3ef3a4c2053f07cc1b28c9f5b014302bcb095e
PulkitAsri <pulkit.asri@gmail.com>
2021-05-21 00:14:35 +0530

    added multiple notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 
 function App(){
-    return<div><Header/><Note/><Footer/></div>;
+    return(
+    <div>
+    <Header/>
+    <div>
+    {notes.map((note)=>(<Note title={note.title} content={note.content}/>))}
+    </div>
+    
+    <Footer/>
+    </div>);
 }
 
 export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-function Note(){
-    return <div className="note"><h1>Title</h1><p>Note taking very good.</p></div> ;
+function Note(props){
+    return (
+    <div className="note">
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
+    </div>
+    );
     
 }
 
